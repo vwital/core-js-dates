ced445c4792b0fcd03f2b5d412418d429d3d0bc3
vwital <wital128@tut.by>
2024-03-20 13:02:51 +0300

    feat: formatDate solved

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -163,8 +163,18 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const d = new Date(date);
+  const result = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'UTC',
+  }).format(d);
+  return result;
 }
 
 /**
